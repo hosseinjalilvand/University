eee650f9ffb482f47dfd54ca2878abd6838b487a
hossein <60768885+hosseinjalilvand@users.noreply.github.com>
2023-06-01 13:13:24 +0330

    Delete Chapter3 directory

--- Chapter3/3.1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-r = eval(input("Enter the  length from the center of a pentagon: "))
-
-s = 2 * r * math.sin(3.1415/5)
-
-Area = (3 * math.sqrt(3) / 2) * s**2 
-
-print("The area of the pentagon is",int(Area*100)/100)
--- Chapter3/3.6.py
+++
@@ -1,5 +0,0 @@
-ascii_code = int(input("Enter an ASCII code (between 0 and 127): "))
-
-character = chr(ascii_code)
-
-print("The character corresponding to the ASCII code", ascii_code, "is", character)
